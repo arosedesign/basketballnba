c0b9791c805ddcc8ab0f260989d63c7098f10143
Luca Vivaldi <vivaldi@edidomus.it>
2017-09-26 18:17:44 +0200

    visual composer

--- wp-content/themes/arosedesign/functions.php
+++ wp-content/themes/arosedesign/functions.php
@@ -1,25 +1,7 @@
 <?php
-/**
- * Arosedesign Theme functions and definitions
- *
- */
-
-if (!function_exists('arosedesign_css')) {
-
-	add_action('wp_print_styles', 'arosedesign_css');
-
-	function arosedesign_css() {
-
-		if (!is_admin()) {
-
-			wp_enqueue_style( 'arosedesign_css-styles', get_stylesheet_directory_uri() . '/style.css' );
-
-			if (is_rtl()) {
-				wp_enqueue_style( 'arosedesign_css-styles-rtl', get_stylesheet_directory_uri() . '/rtl.css' );
-			}
-
-		}
-
-	}
+add_action( 'wp_enqueue_scripts', 'arosedesign_styles' );
+function arosedesign_styles() {
+    wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+?>
