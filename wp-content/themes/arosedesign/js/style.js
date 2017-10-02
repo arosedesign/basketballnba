55a2381498d346cd0ce2ff79593d8b5746da9bc4
Luca Vivaldi <vivaldi@edidomus.it>
2017-10-02 15:59:09 +0200

    Elimina temi

--- wp-content/themes/arosedesign/js/style.js
+++ wp-content/themes/arosedesign/js/style.js
@@ -1,8 +1,19 @@
 jQuery(document).ready(function(){
+
     jQuery('.opensearch').click(function(e) {
         e.preventDefault();
         jQuery( "form.search-form" ).toggleClass( 'opened' );
         jQuery( ".opensearch .fa" ).toggleClass('fa-search');
         jQuery( ".opensearch .fa" ).toggleClass('fa-close');
     });
+
+    mobileupdate()
 });
+
+function mobileupdate(){
+    if(jQuery(window).innerWidth() <= 767) {
+        jQuery('#social_header ul').append(jQuery('a.opensearch'))
+    }
+}
+
+jQuery(window).resize(mobileupdate);
--- wp-content/themes/twentyseventeen/page.php
+++ wp-content/themes/twentyseventeen/page.php
@@ -24,9 +24,29 @@ get_header(); ?>
 			<?php
 			while ( have_posts() ) : the_post();
 
-				get_template_part( 'template-parts/page/content', 'page' );
+				?>
 
-				// If comments are open or we have at least one comment, load up the comment template.
+                <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+                    <div class="entry-content">
+
+                        <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+                        <?php twentyseventeen_edit_link( get_the_ID() ); ?>
+
+                        <?php
+                        the_content();
+
+                        wp_link_pages( array(
+                            'before' => '<div class="page-links">' . __( 'Pages:', 'twentyseventeen' ),
+                            'after'  => '</div>',
+                        ) );
+                        ?>
+                    </div><!-- .entry-content -->
+                </article><!-- #post-## -->
+
+
+            <?php
+
+            // If comments are open or we have at least one comment, load up the comment template.
 				if ( comments_open() || get_comments_number() ) :
 					comments_template();
 				endif;
