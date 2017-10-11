c8cccea2c18b123bacbc1ab4401e7f6c80bef55d
Luca Vivaldi <vivaldi@edidomus.it>
2017-10-11 17:53:39 +0200

    Fine homepage e delete featured in articles

--- wp-content/themes/arosedesign/arosedesign_siteorigin/rss-widget/rss-templates/rss-template.php
+++ wp-content/themes/arosedesign/arosedesign_siteorigin/rss-widget/rss-templates/rss-template.php
@@ -1,6 +1,23 @@
-<div>
-    <?php echo wp_kses_post($instance['rss_url']) ?>
-</div>
+<?php
 
+    $xml=(wp_kses_post($instance['rss_url']));
 
+    $xmlDoc = new DOMDocument();
+    $xmlDoc->load($xml);
+    $num = wp_kses_post($instance['numero_feed']);
 
+    $x=$xmlDoc->getElementsByTagName('item');
+    for ($i=0; $i<$num; $i++) {
+        $item_title=$x->item($i)->getElementsByTagName('title')
+            ->item(0)->childNodes->item(0)->nodeValue;
+        $item_link=$x->item($i)->getElementsByTagName('link')
+            ->item(0)->childNodes->item(0)->nodeValue;
+        $item_desc=$x->item($i)->getElementsByTagName('description')
+            ->item(0)->childNodes->item(0)->nodeValue;
+        echo ("<div class='rssfeed'><h3><a href='" . $item_link
+            . "'>" . $item_title . "</a></h3>");
+        echo ("<a target ='_blank' title='BasketballNCAA - ". $item_title ."' href='" . $item_link
+            . "'><p>" . $item_desc . "</a></p></div>");
+    }
+
+?>
--- wp-content/themes/arosedesign/loop.php
+++ wp-content/themes/arosedesign/loop.php
@@ -1,8 +1,7 @@
 <?php
 /**
- * Just displays a post loop. Intended to be included in child themes using get_template_part('loop'). Also works with SiteOrigin page builder loop widget.
  *
- * Loop Name: Blog Loop
+ * Loop Name: LoopHome
  *
  * @package WordPress
  * @subpackage AroseDesign
@@ -11,12 +10,34 @@
 ?>
 <?php if ( have_posts() ) : ?>
 
+    <script type="text/javascript">
+        function Socialpopup(url) {
+            var w = 500;
+            var h = 360
+            var left = (window.innerWidth / 2) - (w/2);
+            var top =  (window.innerHeight / 2) - (h/2);
+            window.open( url, "Condividi", "status = 1, height = " + h + ", width = " + w + ", top=" + top + ", left=" + left + ", resizable = 0" )
+        }
+    </script>
+
     <?php while ( have_posts() ) : the_post(); ?>
 
-        <h1><a title="<?php the_title(); ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
-        <div class="apertura"><?php the_post_thumbnail(); ?></div>
-        <p class="abstract"><?php the_excerpt(); ?></p>
-        <div class="tags"><?php the_tags('',', ',''); ?></div>
+        <div class="loop bloccoloop">
+
+            <div class="apertura"><a title="<?php the_title(); ?>" href="<?php the_permalink(); ?>"><?php the_post_thumbnail(); ?></a></div>
+            <div class="bloccoinfo">
+                <h1 class="title"><a title="<?php the_title(); ?>" href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h1>
+                <p class="abstract"><?php the_excerpt(); ?></p>
+
+
+                <div class="sharer">
+                    <a title="Facebook" href="javascript:Socialpopup('https://www.facebook.com/sharer/sharer.php?u=<?php urlencode(the_permalink()); ?>')"><i class="fa fa-facebook"></i></a>
+                    <a title="Facebook" href="javascript:Socialpopup('http://twitter.com/share?text=<?php the_title(); ?>&url=<?php urlencode(the_permalink()); ?>moci')"><i class="fa fa-twitter"></i></a>
+                </div>
+                <div class="tags"><?php the_tags('',', ',''); ?></div>
+            </div>
+
+        </div>
 
     <?php endwhile; ?>
 
