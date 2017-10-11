2102494ecf0d973ee6cb01fef5376a331f54937e
Luca Vivaldi <vivaldi@edidomus.it>
2017-10-11 13:16:59 +0200

    Fix custom plugin

--- wp-content/plugins/arosedesign_siteorigin/arosedesign_siteorigin.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-function custom_plugin_arosedesign($folders){
-    $folders[] = plugin_dir_path(__FILE__).'rss-basketballncaa/';
-    return $folders;
-}
-add_filter('siteorigin_widgets_widget_folders', 'custom_plugin_arosedesign');
-
