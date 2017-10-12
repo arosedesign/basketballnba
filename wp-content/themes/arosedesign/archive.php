58f96de6fc7ecfec325a9152b2278a1fe4e11cd1
Luca Vivaldi <vivaldi@edidomus.it>
2017-10-12 12:34:26 +0200

    Archive

--- wp-content/themes/arosedesign/archive.php
+++ wp-content/themes/arosedesign/archive.php
@@ -4,7 +4,7 @@
  *
  * @link https://codex.wordpress.org/Template_Hierarchy
  *
- * @package WordPress
+ * @package ArosedesignTemplate
  * @subpackage Twenty_Seventeen
  * @since 1.0
  * @version 1.0
@@ -12,50 +12,102 @@
 
 get_header(); ?>
 
-<div class="wrap">
+    <div class="wrap">
 
-	<?php if ( have_posts() ) : ?>
-		<header class="page-header">
-			<?php
-				the_archive_title( '<h1 class="page-title">', '</h1>' );
-				the_archive_description( '<div class="taxonomy-description">', '</div>' );
-			?>
-		</header><!-- .page-header -->
-	<?php endif; ?>
+        <?php if ( have_posts() ) : ?>
+            <header class="page-header">
+                <?php
+                the_archive_title( '<h1 class="page-title">', '</h1>' );
+                the_archive_description( '<div class="taxonomy-description">', '</div>' );
+                ?>
+            </header><!-- .page-header -->
+        <?php endif; ?>
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+        <div id="primary" class="content-area">
+            <main id="main" class="site-main" role="main">
 
-		<?php
-		if ( have_posts() ) : ?>
-			<?php
-			/* Start the Loop */
-			while ( have_posts() ) : the_post();
+                <?php
+                if ( have_posts() ) : ?>
+                    <?php
+                    /* Start the Loop */
+                    while ( have_posts() ) : the_post(); ?>
 
-				/*
-				 * Include the Post-Format-specific template for the content.
-				 * If you want to override this in a child theme, then include a file
-				 * called content-___.php (where ___ is the Post Format name) and that will be used instead.
-				 */
-				get_template_part( 'template-parts/post/content', get_post_format() );
+                        <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
 
-			endwhile;
+                            <div class="thumb">
+                                <a title="<?php the_title(); ?>" href="<?php the_permalink(); ?>">
+                                    <?php the_post_thumbnail(); ?>
+                                </a>
+                            </div>
 
-			the_posts_pagination( array(
-				'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
-				'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
-				'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
-			) );
+                            <div class="spiega">
+                                <header class="entry-header">
+                                    <?php if ( 'post' === get_post_type() ) : ?>
+                                        <div class="entry-meta">
+                                            <?php
+                                            echo twentyseventeen_time_link();
+                                            twentyseventeen_edit_link();
+                                            ?>
+                                        </div><!-- .entry-meta -->
+                                    <?php elseif ( 'page' === get_post_type() && get_edit_post_link() ) : ?>
+                                        <div class="entry-meta">
+                                            <?php twentyseventeen_edit_link(); ?>
+                                        </div><!-- .entry-meta -->
+                                    <?php endif; ?>
 
-		else :
+                                    <?php if ( is_front_page() && ! is_home() ) {
 
-			get_template_part( 'template-parts/post/content', 'none' );
+                                        // The excerpt is being displayed within a front page section, so it's a lower hierarchy than h2.
+                                        the_title( sprintf( '<h3 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h3>' );
+                                    } else {
+                                        the_title( sprintf( '<h2 class="entry-title"><a href="%s" rel="bookmark">', esc_url( get_permalink() ) ), '</a></h2>' );
+                                    } ?>
+                                </header><!-- .entry-header -->
 
-		endif; ?>
+                                <div class="entry-summary">
+                                    <?php the_excerpt(); ?>
+                                </div><!-- .entry-summary -->
+                            </div>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
-	<?php get_sidebar(); ?>
-</div><!-- .wrap -->
+
+                        </article><!-- #post-## -->
+
+                    <?php endwhile;
+
+                    the_posts_pagination( array(
+                        'prev_text' => twentyseventeen_get_svg( array( 'icon' => 'arrow-left' ) ) . '<span class="screen-reader-text">' . __( 'Previous page', 'twentyseventeen' ) . '</span>',
+                        'next_text' => '<span class="screen-reader-text">' . __( 'Next page', 'twentyseventeen' ) . '</span>' . twentyseventeen_get_svg( array( 'icon' => 'arrow-right' ) ),
+                        'before_page_number' => '<span class="meta-nav screen-reader-text">' . __( 'Page', 'twentyseventeen' ) . ' </span>',
+                    ) );
+
+                else : ?>
+
+                    <section class="no-results not-found">
+                        <header class="page-header">
+                            <h1 class="page-title"><?php _e( 'Nothing Found', 'twentyseventeen' ); ?></h1>
+                        </header>
+                        <div class="page-content">
+                            <?php
+                            if ( is_home() && current_user_can( 'publish_posts' ) ) : ?>
+
+                                <p><?php printf( __( 'Ready to publish your first post? <a href="%1$s">Get started here</a>.', 'twentyseventeen' ), esc_url( admin_url( 'post-new.php' ) ) ); ?></p>
+
+                            <?php else : ?>
+
+                                <p><?php _e( 'It seems we can&rsquo;t find what you&rsquo;re looking for. Perhaps searching can help.', 'twentyseventeen' ); ?></p>
+                                <?php
+                                get_search_form();
+
+                            endif; ?>
+                        </div><!-- .page-content -->
+                    </section><!-- .no-results -->
+
+
+                <?php endif; ?>
+
+            </main><!-- #main -->
+        </div><!-- #primary -->
+        <?php get_sidebar(); ?>
+    </div><!-- .wrap -->
 
 <?php get_footer();
