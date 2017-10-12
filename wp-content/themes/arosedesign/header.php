253bcc96c80e219747b125473843abba4ebe9628
Luca Vivaldi <vivaldi@edidomus.it>
2017-10-12 19:21:42 +0200

    FB App ID

--- wp-content/themes/arosedesign/header.php
+++ wp-content/themes/arosedesign/header.php
@@ -47,6 +47,24 @@
     gtag('config', 'UA-83590033-2');
 </script>
 
+<script>
+    window.fbAsyncInit = function() {
+        FB.init({
+            appId      : '1946564005556988',
+            xfbml      : true,
+            version    : 'v2.10'
+        });
+        FB.AppEvents.logPageView();
+    };
+
+    (function(d, s, id){
+        var js, fjs = d.getElementsByTagName(s)[0];
+        if (d.getElementById(id)) {return;}
+        js = d.createElement(s); js.id = id;
+        js.src = "//connect.facebook.net/en_US/sdk.js";
+        fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));
+</script>
 
 <div id="page" class="site">
 
